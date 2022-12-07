66bc787fac6caedf6580eb0f3ec3d148802063df
Daniel Almeida <daniel.almeida11@kraftheinz.com>
2022-12-07 17:14:48 -0300

    Simple empty state

--- src/pages/PageEmptyState.tsx
+++ src/pages/PageEmptyState.tsx
@@ -4,11 +4,16 @@ import { RouteParams } from "src/types/request";
 const EmptyState = () => {
   const { pokemonName } = useParams<RouteParams>();
 
-  return (
-    <div className="flex h-full justify-center items-center">
-      <h1 className="text-5xl font-bold">Pokémon {pokemonName} not found</h1>
-    </div>
-  );
+    return (
+      <div className="flex h-full justify-center items-center p-10">
+        <h1 className="text-5xl font-bold text-center">
+          {pokemonName
+            ? `Pokémon ${pokemonName} not found`
+            : "Select or find a Pokémon for more details"}
+        </h1>
+      </div>
+    );
+
 };
 
 export default EmptyState;
